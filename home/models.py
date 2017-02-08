572435272b0d964bd98269f51a9b4cb899feebbb
Prawin <theincredible13@gmail.com>
2017-02-08 17:39:40 +0530

    Created Models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class submissions(models.Model):
+	name = models.CharField((u"Name"),max_length=200)
+	email = models.CharField((u"Email-ID"),max_length=200)
+	subject = models.CharField((u"Subject"),max_length=200)
+	message = models.CharField((u"Message"),max_length=10000)
+
+	class Meta:
+		verbose_name = 'Submissions'
+		verbose_name_plural = 'Submissions'
+
+	def __str__(self):
+		return self.Name+' - '+self.email+'-'+self.subject
