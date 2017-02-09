85cc2bd6387430010329d512f393092423b88220
Prawin <theincredible13@gmail.com>
2017-02-09 18:10:44 +0530

    changed contents

--- home/static/js/grid.js
+++ home/static/js/grid.js
@@ -1,12 +1,3 @@
-/*
-* debouncedresize: special jQuery event that happens once after a window resize
-*
-* latest version and complete README available on Github:
-* https://github.com/louisremi/jquery-smartresize/blob/master/jquery.debouncedresize.js
-*
-* Copyright 2011 @louis_remi
-* Licensed under the MIT license.
-*/
 var $event = $.event,
 $special,
 resizeTimeout;
@@ -344,7 +335,7 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
-			this.$href = $( '<a href="#">Visit website</a>' );
+			this.$href = $( '' );
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import *
 from django.http import *
-from django.shortcuts import render
 from django.template.response import TemplateResponse
 # Create your views here.
 
 def index(request):
 	return render(request,'index.html',{})
+
+
+def sendmail(request):
+	name = request.POST['name']
+	email = request.POST['email']
+	subject = request.POST['subject']
+	message = request.POST['message']
+	return redirect('index')
